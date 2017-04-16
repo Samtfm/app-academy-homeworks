3ddfc2a01ea45c4eee9ad468be4f68aaea117733
Samtfm <sfmemail@gmail.com>
2017-04-16 12:48:30 -0700

    finish controller excercise

--- W4D1/controllers/app/controllers/books_controller.rb
+++ W4D1/controllers/app/controllers/books_controller.rb
@@ -1,18 +1,30 @@
 class BooksController < ApplicationController
   def index
     # your code here
+    @books = Book.all
+    render :index
   end
 
   def new
     # your code here
+    render :new
   end
 
   def create
+    book = Book.new(book_params)
+    book.save
+    index
     # your code here
   end
 
   def destroy
     # your code here
+    book = Book.find_by_id(params[:id])
+    if book.destroy
+      index
+    else
+      throw fail
+    end
   end
 
   private
